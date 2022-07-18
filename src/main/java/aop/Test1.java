6724a64204de2c5ca2f87ff9349082c6e43161c0
Yuriy Novikov <tow1987@yandex.ru>
2022-07-18 15:30:57 +0300

    Add AOP

---
+++ src/main/java/aop/Test1.java
@@ -0,0 +1,15 @@
+package aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class)) {
+
+            Library library = context.getBean("libraryBean", Library.class);
+            library.getBook();
+
+        }
+    }
+}
