ac8d6251565fb83aa294c9fae63d17501dee7b12
Yuriy Novikov <tow1987@yandex.ru>
2022-07-19 18:22:09 +0300

    Change properties file of log4j to xml file

--- src/main/java/aop/MyConfig.java
+++ src/main/java/aop/MyConfig.java
@@ -1,7 +1,6 @@
 package aop;
 
 
-import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -1,6 +1,5 @@
 package aop;
 
-import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,9 +7,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test1 {
     public static void main(String[] args) {
 
-        PropertyConfigurator.configure("C:\\Users\\TOW\\IdeaProjects\\spring_course\\resources\\log4j.properties");
-        Logger logger = LoggerFactory.getLogger(Logger.class);
-        logger.error("hello world!");
+        Logger logger = LoggerFactory.getLogger(Test1.class);
+        logger.info("hello world!");
 
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class)) {
 
